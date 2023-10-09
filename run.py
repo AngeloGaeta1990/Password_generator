a1a1036a3349b356fe44d7ccbe4df34d9dd962ac
Angelo Gaeta <131547377+AngeloGaeta1990@users.noreply.github.com>
2023-10-09 22:49:19 +0100

    style: remove run.py code instute comments

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 import csv
 import os
 from classes.account import Account
